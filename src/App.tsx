af0ffbdae24f1ac0a5fef7426b2a5e31b5e227a9
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-28 17:50:38 -0500

    add input in App, change name of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import HelloWorld from './components/EditableView';
 
 function App() {
+
+  const defaultText = 'Edit src/App.jsx and save to reload.';
+  const logo: string = require("./logo.svg").default;
+  const [text, setText] = useState(defaultText);
+
+  
   return (
     <div className="App">
+      <HelloWorld />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+        <input type="text" onChange={(e) => setText(e.target.value)} />
+        <p className='App-text'>{text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
