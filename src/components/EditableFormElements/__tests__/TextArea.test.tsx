af6b11e1564e3048b46d6b1242493c91e5e1217a
Greg Tambornino <greg.tambornino@concordusa.com>
2023-03-31 17:14:03 -0500

    include basic render test for TextInput component

--- src/components/EditableFormElements/__tests__/TextArea.test.tsx
+++ src/components/EditableFormElements/__tests__/TextArea.test.tsx
@@ -4,7 +4,14 @@ import { EditableTextArea } from "../TextArea";
 describe('EditableTextArea', () => {
     // beforeAll()
     test('should render a text element', () => {
-        render(<EditableTextArea />);
+        const passable = [{
+            editMode: false,
+            setEditMode: jest.fn(),
+            userText: 'Click here and type some text',
+            setUserText: jest.fn(),
+            inputRef: { current: null }
+        }];
+        render(<EditableTextArea inherited={passable} elIndex={0} />);
         const element = screen.getByTestId("editableArea");
         expect(element).toBeInTheDocument();
     });
--- src/components/EditableFormElements/__tests__/TextInput.test.tsx
+++ src/components/EditableFormElements/__tests__/TextInput.test.tsx
@@ -1,3 +1,15 @@
 import { render, screen } from "@testing-library/react";
 import { EditableTextField } from '../TextInput';
 
+describe('EditableTextField', () => {
+    test('should render', () => {
+        const passable = [{
+            editMode: false,
+            setEditMode: jest.fn(),
+            userText: 'Click here and type some text',
+            setUserText: jest.fn(),
+            inputRef: { current: null }
+        }];
+        expect(() => render(<EditableTextField inherited={passable} elIndex={0} />)).not.toThrow();
+    });
+});
