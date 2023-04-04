b088aee0422f98f9da869529befadbca32d036d9
Greg Tambornino <greg.tambornino@concordusa.com>
2023-04-04 14:16:06 -0500

    test input functionality

--- src/components/EditableFormElements/__tests__/TextInput.test.tsx
+++ src/components/EditableFormElements/__tests__/TextInput.test.tsx
@@ -1,55 +1,38 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { InputProps } from "../../EditableView/ControlView";
 
 import userEvent from "@testing-library/user-event";
 import { EditableTextField } from '../TextInput';
 
+
 describe('EditableTextField', () => {
+    
+    const passable: InputProps = {
+        editMode: false,
+        setEditMode: jest.fn(),
+        userText: 'Click here and type some text',
+        setUserText: jest.fn(),
+        inputRef: { current: null }
+    };
+
     test('should render', () => {
-        const passable: InputProps = {
-            editMode: false,
-            setEditMode: jest.fn(),
-            userText: 'Click here and type some text',
-            setUserText: jest.fn(),
-            inputRef: { current: null }
-        };
+        
         expect(() => render(<EditableTextField inherited={passable} elIndex={0} />)).not.toThrow();
     });
     test('should show a text field if "editMode" is false', async () => {
-        let passable: InputProps = {
-            editMode: false,
-            setEditMode: jest.fn(),
-            userText: 'Click here and type some text',
-            setUserText: jest.fn(),
-            inputRef: { current: null }
-        };
         render(<EditableTextField inherited={passable} elIndex={0} />);
         const textField = await screen.findByTestId('editable-text');
         expect(textField).toBeInTheDocument();
     });
     test('should show an input if "editMode" is true', async () => {
-        let passable:InputProps = {
-            editMode: true,
-            setEditMode: jest.fn(),
-            userText: 'Click here and type some text',
-            setUserText: jest.fn(),
-            inputRef: { current: null }
-        };
+        passable.editMode = true 
         render(<EditableTextField inherited={passable} elIndex={0} />);
         const textField = await screen.findByTestId('editable-input');
         expect(textField).toBeInTheDocument();
     });
     test('should show not show the text field if blank', async () => {
-        let editMode = true;
-        const setEditMode = jest.fn();
-        const setUserText = jest.fn();
-        const passable = {
-            editMode: editMode,
-            setEditMode: setEditMode,
-            userText: '',
-            setUserText: setUserText,
-            inputRef: { current: null }
-        };
+        passable.editMode = true;
+        passable.userText = '';
         render(<EditableTextField inherited={passable} elIndex={0} />)
         const textElement = await screen.findByTestId('editable-input')
         userEvent.click(textElement);
@@ -57,5 +40,26 @@ describe('EditableTextField', () => {
         userEvent.keyboard('{Backspace>31}');
         userEvent.keyboard('{Enter}');
         expect(screen.queryByTestId('editable-text')).not.toBeInTheDocument()
+        expect(screen.queryByTestId('editable-input')).toBeInTheDocument();
+    });
+    test("should initialize with 'hiya buddy!' when passed 'hiya buddy!'", async () => {
+        passable.editMode = false;
+        passable.userText = 'hiya buddy!'
+        render(<EditableTextField inherited={passable} elIndex={0} />)
+        expect(screen.queryByText('hiya buddy!')).toBeInTheDocument();
+    });
+    test("should show 'hiya back buddy!' when entered into field", async () => {
+        passable.editMode = true;
+        passable.userText = 'Click here and type some text'
+        render(<EditableTextField inherited={passable} elIndex={0} />);
+        // const textElement = await screen.findByTestId('editable-text')
+        const inputField = await screen.findByTestId('editable-input') as HTMLInputElement;
+        fireEvent.change(inputField, 
+        { target: { value: 'hiya back buddy!'},});
+        // Aternate method of doing the same thing.
+        // userEvent.click(inputField);
+        // userEvent.keyboard('hiya back buddy!');
+        // userEvent.keyboard('{Enter}');
+        expect(screen.getByDisplayValue('hiya back buddy!')).toBeInTheDocument();
     });
 });
--- src/components/EditableView/__tests__/ControlView.test.tsx
+++ src/components/EditableView/__tests__/ControlView.test.tsx
@@ -1,8 +1,11 @@
+import React, { Dispatch, SetStateAction } from "react";
 import { render, screen } from "@testing-library/react";
 import { EditableTextField } from "../../EditableFormElements/TextInput";
-import ControlView from "../ControlView";
+import ControlView, { InputProps } from "../ControlView";
 
 jest.mock('../../EditableFormElements/TextInput');
+
+
 describe('ControlView', () => {
     // beforeAll(() => {
         
@@ -12,21 +15,11 @@ describe('ControlView', () => {
         expect(() => render(<ControlView />)).not.toThrow();
     });
     test('should not render mocked child ', () => {
+
         render(<ControlView />);
         let childElement = screen.queryByText(/EditableTextField/i);
         expect(childElement).not.toBeInTheDocument();
-        const passable = [{
-            editMode: false,
-            setEditMode: jest.fn(),
-            userText: 'Click here and type some text',
-            setUserText: jest.fn(),
-            inputRef: { current: null }
-        }];
-        const elIndex = 0
-        // expect(EditableTextField).toHaveBeenCalledWith(
-        //     expect.objectContaining({elIndex: elIndex, inherited: passable }), 
-        //     expect.anything()
-        //     )
     });
+    
 });
 
